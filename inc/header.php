a0a931100d952b4f5a643b24d36286f4d4484f18
Diogo Oliveira <dihhg34@gmail.com>
2021-06-22 12:31:13 -0300

    navbar inserida

--- inc/header.php
+++ inc/header.php
@@ -1,5 +1,29 @@
-</body>
-
-<header>
-    
-</header>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php">
+                    <img src="https://w7.pngwing.com/pngs/181/882/png-transparent-insurance-agent-house-real-estate-business-houses-silhouette-angle-logo-insurance.png" alt="logo" whidh="30px" height="30">
+                    THE BROTHERS</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Fotos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contato</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
